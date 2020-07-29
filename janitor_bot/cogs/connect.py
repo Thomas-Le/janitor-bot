6f173355625ac227ad8bb400f5ce0997f2e33453
brickgoat <brickgoat@gmail.com>
2020-07-29 13:32:23 -0400

    Error handling for command not found

--- janitor_bot/cogs/connect.py
+++ janitor_bot/cogs/connect.py
@@ -5,10 +5,14 @@ class Connect(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Event
     @commands.Cog.listener()
     async def on_ready(self):
         print("Bot is connected!")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("Command not found.\nUse '!help' to see list of commands.")
 
 
 def setup(bot):
