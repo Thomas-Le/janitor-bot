b5dc80158495f4c5bd099a0386dc8f68f633465f
brickgoat <brickgoat@gmail.com>
2020-07-30 19:46:52 -0400

    added end-replacement command w/ error handling

--- janitor_bot/cogs/replace.py
+++ janitor_bot/cogs/replace.py
@@ -16,7 +16,7 @@ class Replace(commands.Cog):
             f"^{self.commandPrefix}", message.content
         ):
             return
-        new_message = ""
+        new_message = message.content
         for word in keywords:
             if re.search(word, message.content):
                 new_message = sendReplacement(message.content)
@@ -25,14 +25,28 @@ class Replace(commands.Cog):
         if new_message != message.content:
             await message.channel.send(f"{message.author} : {new_message}")
 
-    @commands.command(help ="Command janitor bot to replace the first specified word with the second in all future messages.\nStop replacements with 'end-replacement <arg1>'")
+    @commands.command(
+        help="Command janitor bot to replace the first specified word with the second in all future messages.\nStop replacements with '!end-replacement <arg1>'"
+    )
     async def replace(self, ctx, arg1: str, arg2: str):
         setReplacement(arg1, arg2)
 
     @replace.error
     async def replace_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"{error} Use '!help replace' for more info.")
+        await ctx.send(f"{error} Use '!help replace' for more info.")
+
+    @commands.command(
+        name="end-replacement", help="Command janitor bot to stop replacing a word."
+    )
+    async def end_replacement(self, ctx, arg1: str):
+        remove_replacement(arg1)
+
+    @end_replacement.error
+    async def end_replacement_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send(f"{error.original}")
+        else:
+            await ctx.send(f"{error} Use '!help end-replacement' for more info.")
 
 
 def setup(bot):
@@ -44,6 +58,16 @@ def sendReplacement(message):
     for word in keywords:
         new_message = re.sub(word, keywords.get(word), new_message)
     return new_message
+
+
+def remove_replacement(keyword):
+    global keywords
+    try:
+        keywords.pop(keyword)
+    except KeyError:
+        raise commands.CommandInvokeError(
+            "I am currently not replacing that word!\nUse '!show-replacements' to see a list of words and their replacement."
+        )
 
 
 def setReplacement(keyword, val):
@@ -59,4 +83,3 @@ def sort_dict():
             keywords.items(), key=lambda item: item[0], reverse=True
         )
     }
-    
